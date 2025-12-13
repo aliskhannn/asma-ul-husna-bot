f2db80c5dc0532287e355f81171ca842f5abfc95
Aliskhan <aliskhanreventon@gmail.com>
2025-12-13 17:14:35 +0300

    refactor(user usecase): add user creation logic to EnsureUser method

--- internal/usecase/user.go
+++ internal/usecase/user.go
@@ -18,6 +18,15 @@ func NewUserUseCase(repo UserRepository) *UserUseCase {
 	return &UserUseCase{repo: repo}
 }
 
-func (uc *UserUseCase) EnsureUser(ctx context.Context, user *entities.User) error {
+func (uc *UserUseCase) EnsureUser(
+	ctx context.Context,
+	userID int64,
+	firstName string,
+	lastName string,
+	username string,
+	languageCode string,
+	isActive bool,
+) error {
+	user := entities.NewUser(userID, firstName, lastName, username, languageCode, isActive)
 	return uc.repo.EnsureUser(ctx, user)
 }
