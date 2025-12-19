3dec3dfe3b2623858665ea4fba9da4a3c3e80ab3
Aliskhan <aliskhanreventon@gmail.com>
2025-12-20 01:37:30 +0300

    fix(entrypoint): use absolute goose path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-goose -dir ./migrations postgres "$DATABASE_URL" up
+/usr/local/bin/goose -dir ./migrations postgres "$DATABASE_URL" up
 
 echo "Starting bot..."
 exec ./husna-bot
